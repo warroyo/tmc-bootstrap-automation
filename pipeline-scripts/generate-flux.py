4602b11c24356e3898c983763546b105692f64eb
Will Arroyo <warroyo7199008@gmail.com>
2024-04-09 13:27:37 -0600

    add generate to pipeline

---
+++ pipeline-scripts/generate-flux.py
@@ -0,0 +1,18 @@
+from copier import run_copy
+import json
+import sys
+
+filename = sys.argv[1]
+f = open(filename)
+data = json.load(f)
+
+
+for cluster in data["cluster_names"]["value"]:
+    #create structure for clusters
+    # run_copy("../flux-templating", "../flux",data={"new_group": "false","new_cluster": "true","cluster_name": cluster})
+    print(cluster)
+    
+for group in data["cluster_groups"]["value"]:
+    #create structure for cluster groups
+    print(group)
+    # run_copy("../flux-templating", "../flux",data={"new_group": "true","new_cluster": "false","cluster_group": group})
