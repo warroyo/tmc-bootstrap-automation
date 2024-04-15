4bd5ff1861dabc610333f355e3f4c80459712ce3
Will Arroyo <warroyo7199008@gmail.com>
2024-04-15 12:10:48 -0600

    update without overwrites

--- pipeline-scripts/generate-flux.py
+++ pipeline-scripts/generate-flux.py
@@ -9,8 +9,8 @@ data = json.load(f)
 
 for cluster in data["cluster_names"]["value"]:
     #create structure for clusters
-    run_copy("../flux-templating", "../flux",data={"new_group": "false","new_cluster": "true","cluster_name": cluster})
+    run_copy("../flux-templating", "../flux",data={"new_group": "false","new_cluster": "true","cluster_name": cluster},force="true")
 
 for group in data["cluster_groups"]["value"]:
     #create structure for cluster groups
-    run_copy("../flux-templating", "../flux",data={"new_group": "true","new_cluster": "false","cluster_group": group})
+    run_copy("../flux-templating", "../flux",data={"new_group": "true","new_cluster": "false","cluster_group": group},force="true")
